19aed2b6efb5284b7528a1e5b327619eeb62c3ec
Namrebos <dienaszaglis@gmail.com>
2025-04-18 00:37:32 +0300

    samainītas pogas truckselector karte

--- src/TruckSelector.jsx
+++ src/TruckSelector.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import "./TruckSelector.css";
+import React from 'react';
+import './TruckSelector.css';
 
 const TruckSelector = ({ onSelect }) => {
   return (
     <div className="truck-selector-container">
       <h2 className="truck-selector-title">Izvēlies Auto</h2>
       <div className="truck-buttons">
-        <button onClick={() => onSelect("HK8643")}>HK8643</button>
-        <button onClick={() => onSelect("RO3201")}>RO3201</button>
-        <button onClick={() => onSelect("MU466")}>MU466</button>
+        <button className="truck-btn mu466" onClick={() => onSelect('MU466')}>MU466</button>
+        <button className="truck-btn ro3201" onClick={() => onSelect('RO3201')}>RO3201</button>
+        <button className="truck-btn hk8643" onClick={() => onSelect('HK8643')}>HK8643</button>
       </div>
     </div>
   );
